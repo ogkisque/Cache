4041f80e228ed1c503fb9f15a2fb482f874379d7
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2024-09-11 00:06:04 +0300

    lfu fast, tests OK

--- check_tests.py
+++ check_tests.py
@@ -3,7 +3,6 @@ from sys import executable
 
 num_test = 1
 for i in range(1, 11):
-	
 	str_data =  "tests/"
 	if i != 10:
 		str_data += "0"
@@ -13,12 +12,15 @@ for i in range(1, 11):
 	if i != 10:
 		str_ans += "0"
 	str_ans += str(i) + ".dat.ans"
+
 	ans = ""
 	for i in open(str_ans):
 		ans = i
+	
 	result = run("./implementation/cache", capture_output = True, encoding='cp866', stdin=file_in)
 	print("Test: " + str(num_test).strip())
 	print("Result: " + str(result.stdout).strip())
+	
 	if result.stdout == ans:
 		print("OK")
 	else:
--- implementation/include/cache.hpp
+++ implementation/include/cache.hpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <unordered_map>
 #include <iterator>
+#include <iostream>
 
 namespace cache
 {
@@ -26,38 +27,43 @@ namespace cache
         template <typename F> bool find_update(KeyT key, F slow_get_page) {
             auto hit = hash_map_.find(key);
 
+            //std::cout << this;
+
             if (hit == hash_map_.end())
             {
                 if (is_full())
                 {
-                    auto min_it = cache_.begin();
-                    size_t min_count = std::get<1>(*min_it);
-
-                    for (auto it = cache_.begin(); it != cache_.end(); it = std::next(it))
-                    {
-                        size_t count = std::get<1>(*it);
-                        if (count < min_count)
-                        {
-                            min_count = count;
-                            min_it = it;
-                        }
-                    }
-                
-                    hash_map_.erase(std::get<0>(*min_it));
-                    cache_.erase(min_it);
+                    hash_map_.erase(std::get<0>(*cache_.begin()));
+                    cache_.pop_front();
                 }
 
-                cache_.emplace_front(key, slow_get_page(key), 1);
+                cache_.emplace_front(key, 1, slow_get_page(key));
                 hash_map_.emplace(key, cache_.begin());
 
                 return false;
             }
 
             auto list_it = hit->second;
-            std::get<1>(*list_it)++;
+            size_t cur_count = ++std::get<1>(*list_it);
+            auto next_it = std::next(list_it);
             
+            if (next_it != cache_.begin() && next_it != cache_.end())
+            {
+                size_t next_count = std::get<1>(*next_it);
+
+                if (cur_count > next_count)
+                    cache_.splice(list_it, cache_, next_it, std::next(next_it));
+            }
             return true;
         }
+    
+        friend std::ostream& operator <<(std::ostream& cout, const Cache& cache) 
+        {
+            std::cout << "Cache dump:\n";
+            for (auto it = cache.cache_.begin(); it != cache.cache_.end(); it = std::next(it))
+                cout << "data: " << std::get<0>(*it) << " count: " << std::get<1>(*it) << '\n'; 
+            return cout; 
+        } 
     };
 
 } // namespace cache
